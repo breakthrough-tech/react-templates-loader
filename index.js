c5b9716cd4019040f3ae826213cfb1f7320a86ec
Scott Fairbairn <scott@breakthrough-tech.com>
2017-03-06 10:50:52 -0500

    inject readFileSync into options

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var reactTemplates = require('react-templates/src/reactTemplates');
 var loaderUtils = require('loader-utils');
-
+var fs = require('fs');
 module.exports = function(source) {
 	var options = loaderUtils.parseQuery(this.query);
+	options.readFileSync = fs.readFileSync;
 	this.cacheable && this.cacheable();
 	return reactTemplates.convertTemplateToReact(source, options);
 };
